94061578162eed7ba16507b8348f1405edfef22a
Maxime Opsommer <maxime.opsommer@gmail.com>
2017-02-14 00:23:00 +0100

    Manquait les crochets MDR

--- src/main/java/bc_om/car_ftp/log/ConsoleLogger.java
+++ src/main/java/bc_om/car_ftp/log/ConsoleLogger.java
@@ -3,7 +3,7 @@ package bc_om.car_ftp.log;
 public class ConsoleLogger {
 
 	public static void log(LogType type, String message) {
-		System.out.println(type.toString() + message);
+		System.out.println("[" + type.toString() + "]" + message);
 	}
 	
 }
